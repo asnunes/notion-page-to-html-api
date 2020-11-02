1792ed54ba785c95cffe239348db55908d9312a5
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-11-01 22:24:52 -0300

    feat: add cover, icon and title routes

---
+++ api/cover.ts
@@ -0,0 +1,37 @@
+import { NowRequest, NowResponse } from '@vercel/node';
+import NotionPageToHtml from 'notion-page-to-html';
+import { MissingIdError } from '../_errors/missing-id';
+
+export default async (request: NowRequest, response: NowResponse): Promise<void | NowResponse> => {
+  try {
+    const { id } = request.query;
+    if (!id) throw new MissingIdError();
+
+    const url = `https://notion.so/${id}`;
+    const content = await NotionPageToHtml.convert(url);
+    const { cover } = content;
+
+    if (!cover) return response.status(404).send('NO COVER');
+
+    const base64 = cover.split(',')[1];
+    const format = cover.split(':')[1].split(';')[0];
+    const img = Buffer.from(base64, 'base64');
+
+    response.writeHead(200, {
+      'Content-Type': format,
+      'Content-length': img.length,
+    });
+
+    response.end(img);
+  } catch (err) {
+    switch (err.name) {
+      case 'MissingIdError':
+      case 'InvalidPageUrlError':
+        return response.status(400).send(err.message);
+      case 'NotionPageAccessError':
+        return response.status(401).send(err.message);
+      default:
+        return response.status(500).send(err.message);
+    }
+  }
+};
---
+++ api/icon.ts
@@ -0,0 +1,40 @@
+import { NowRequest, NowResponse } from '@vercel/node';
+import NotionPageToHtml from 'notion-page-to-html';
+import { MissingIdError } from '../_errors/missing-id';
+
+export default async (request: NowRequest, response: NowResponse): Promise<void | NowResponse> => {
+  try {
+    const { id } = request.query;
+    if (!id) throw new MissingIdError();
+
+    const url = `https://notion.so/${id}`;
+    const content = await NotionPageToHtml.convert(url);
+    const { icon } = content;
+
+    if (icon.includes('base64')) {
+      const base64 = icon.split(',')[1];
+      const format = icon.split(':')[1].split(';')[0];
+      const img = Buffer.from(base64, 'base64');
+
+      response.writeHead(200, {
+        'Content-Type': format,
+        'Content-length': img.length,
+      });
+
+      return response.end(img);
+    }
+
+    response.setHeader('Content-Type', 'text/plain');
+    response.status(200).send(icon);
+  } catch (err) {
+    switch (err.name) {
+      case 'MissingIdError':
+      case 'InvalidPageUrlError':
+        return response.status(400).send(err.message);
+      case 'NotionPageAccessError':
+        return response.status(401).send(err.message);
+      default:
+        return response.status(500).send(err.message);
+    }
+  }
+};
